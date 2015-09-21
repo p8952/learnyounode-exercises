ec80067d7da16f9de25b86ba59a0ffab2dc73922
Peter Wilmott <p@p8952.info>
2015-09-21 22:37:00 +0100

    exercise_08

---
+++ exercise_08.js
@@ -0,0 +1,10 @@
+var http = require("http");
+var concat = require("concat-stream");
+
+http.get(process.argv[2], function(res) {
+	res.setEncoding("utf8");
+	res.pipe(concat(function (data) {
+		console.log(data.length);
+		console.log(data);
+	}));
+});
